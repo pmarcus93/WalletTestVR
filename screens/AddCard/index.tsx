bc43876238bc5b34b4ee1eaa524daa7d40a62161
Marcus Pereira <pereiramarcus93@gmail.com>
2023-10-13 10:13:28 -0300

    ajustes-formulario: ajusta posição de import.

--- screens/AddCard/index.tsx
+++ screens/AddCard/index.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import {ImageBackground, KeyboardAvoidingView, Platform} from 'react-native';
 
-const backgroundImage = require('../../assets/background.png');
 import globalStyles from '@shared/globalStyles';
 
 import FormCard from '@screens/FormCard';
+
+const backgroundImage = require('../../assets/background.png');
 
 function AddCard() {
   return (
