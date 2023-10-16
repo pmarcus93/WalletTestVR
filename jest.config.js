70911c097f084291307050c4f42b1f4387b84b27
Marcus Pereira <pereiramarcus93@gmail.com>
2023-10-16 13:22:46 -0300

    testes: adiciona config de coverage.

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,11 @@ module.exports = {
   preset: 'react-native',
   setupFiles: ['<rootDir>/jest.setup.js'],
   setupFilesAfterEnv: ["@testing-library/jest-native/extend-expect"],
+  collectCoverageFrom: [
+    '**/*.{js,jsx,ts,tsx}',
+    '!**/node_modules/**',
+    '!**/vendor/**',
+  ],
   moduleNameMapper: {
     '.+\\.(css|styl|less|sass|scss|png|jpg|ttf|woff|woff2)$':
       'identity-obj-proxy',
